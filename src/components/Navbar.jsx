609a84c9704d7500138aad99ec58433a9afaa973
chao lobi <gogoiladitya@gmail.com>
2023-04-20 18:50:54 +0530

    Layout is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,29 +7,25 @@ import icon from '../images/logo.png';
 
 const Navbar = () => {
     return (
-        <div className='nav-container'>
+        <div className="nav-container">
             <div className="logo-container">
-                <Avatar src={icon} size='large' />
-                <Typography.Title level={2} className="logo">
-                    <Link to='/'>19Crypto</Link>
-                    {/* <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button> */}
-                </Typography.Title>
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo"><Link to="/">Cryptoverse</Link></Typography.Title>
             </div>
-
-            {/* <Button className='menu-control-container'>
-    </Button> */}
-            <Menu theme='dark'>
+            <Menu theme="dark">
                 <Menu.Item icon={<HomeOutlined />}>
-                    <Link to='/'>Home</Link>
+                    <Link to="/">Home</Link>
                 </Menu.Item>
                 <Menu.Item icon={<FundOutlined />}>
-                    <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
                 </Menu.Item>
                 <Menu.Item icon={<BulbOutlined />}>
-                    <Link to='/news'>News</Link>
+                    <Link to="/news">News</Link>
                 </Menu.Item>
             </Menu>
-
         </div>
     )
 }
