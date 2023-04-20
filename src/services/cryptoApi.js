b0f2d6e9b7332777c586a501259d817c9b00db6f
chao lobi <gogoiladitya@gmail.com>
2023-04-20 19:53:01 +0530

    redux toolkit and API dev is done

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '1826207d5emshddc1d9adbb77a30p1b471fjsnce00870209e9',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+};
+
+
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+// coins
+// url: 'https://coinranking1.p.rapidapi.com/coins',
+
+// specific coin
+// url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd',
+
+// coin history
+// https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history
+
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
